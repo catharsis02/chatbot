d3b404f0bdeb0284160c303977ae2dc92fdd6701
kanishka <kanishka161203@gmail.com>
2025-09-19 16:19:30 +0530

    Installation

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+echo "Setting up Linux environment..."
 set -e
 
+echo "Setting up Package Manager..."
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
 uv sync
+
+echo "Downloading NLTK data..."
 ./.venv/bin/python3 script.py
-source ./.venv/bin/activate
+
+echo "Setting up Python Virtual Environment..."
+echo "Activate virtual environment using the command:"
+echo "source ./.venv/bin/activate"
