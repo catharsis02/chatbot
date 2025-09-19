b6084c566cabf1250a3b1516f101c9cd66e572be
kanishka <kanishka161203@gmail.com>
2025-09-19 13:26:29 +0530

    Installation

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 set -e
 
-# Create virtual environment
-python3 -m venv .venv
-
-# Activate virtual environment
-source .venv/bin/activate
-
-# Upgrade pip (optional but recommended)
-python -m pip install --upgrade pip
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Run the script
-python script.py
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv sync
